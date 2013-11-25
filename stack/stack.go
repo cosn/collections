e80ddfedbab5c8ef1a228c89da54b15679f398e3
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2013-11-24 16:08:56 -0800

    revert indentation changes

--- stack/stack.go
+++ stack/stack.go
@@ -3,64 +3,64 @@ package stack
 
 // stack is the internal representation of the data structure
 type stack struct {
-        storage []interface{}
-        i       int
+	storage []interface{}
+	i       int
 }
 
 // New returns an initialized stack of a given size
 func New(size int) (s *stack) {
-        s = new(stack)
-        s.storage = make([]interface{}, size)
-        s.i = -1
+	s = new(stack)
+	s.storage = make([]interface{}, size)
+	s.i = -1
 
-        return
+	return
 }
 
 // Push adds a new element to the top of the stack
 // O(1)
 func (s *stack) Push(v interface{}) {
-        // dynamically increase the size of storage as needed
-        if s.i+1 == cap(s.storage) {
-                ns := make([]interface{}, s.i*2)
-                copy(ns, s.storage)
-                s.storage = ns
-        }
+	// dynamically increase the size of storage as needed
+	if s.i+1 == cap(s.storage) {
+		ns := make([]interface{}, s.i*2)
+		copy(ns, s.storage)
+		s.storage = ns
+	}
 
-        s.i++
-        s.storage[s.i] = v
+	s.i++
+	s.storage[s.i] = v
 }
 
 // Pop removes the top element from the stack
 // O(1).
 func (s *stack) Pop() interface{} {
-        if s.i < 0 {
-                return nil
-        }
+	if s.i < 0 {
+		return nil
+	}
 
-        v := s.storage[s.i]
-        s.i--
+	v := s.storage[s.i]
+	s.i--
 
-        return v
+	return v
 }
 
 // Peek returns the top element from the stack without removing it
 // O(1)
 func (s *stack) Peek() interface{} {
-        if s.i < 0 {
-                return nil
-        }
+	if s.i < 0 {
+		return nil
+	}
 
-        return s.storage[s.i]
+	return s.storage[s.i]
 }
 
 // IsEmpty returns a value indicating whether the stack has any elements
 // O(1)
 func (s *stack) IsEmpty() bool {
-        return s.Len() == 0
+	return s.Len() == 0
 }
 
 // Len returns the number of elements in the stack
 // O(1)
 func (s *stack) Len() int {
-        return s.i + 1
+	return s.i + 1
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -5,72 +5,72 @@ import "testing"
 const iterations = 1024
 
 func TestPushPop(t *testing.T) {
-        s := New(iterations / 2)
+	s := New(iterations / 2)
 
-        for i := 0; i < iterations; i++ {
-                s.Push(i)
-        }
+	for i := 0; i < iterations; i++ {
+		s.Push(i)
+	}
 
-        for i := iterations - 1; i <= 0; i-- {
-                testPop(t, s, i)
-        }
+	for i := iterations - 1; i <= 0; i-- {
+		testPop(t, s, i)
+	}
 }
 
 func TestPeek(t *testing.T) {
-        s := New(10)
+	s := New(10)
 
-        s.Push("a")
-        testPeek(t, s, "a")
+	s.Push("a")
+	testPeek(t, s, "a")
 
-        s.Push("b")
-        testPeek(t, s, "b")
+	s.Push("b")
+	testPeek(t, s, "b")
 
-        s.Pop()
-        testPeek(t, s, "a")
+	s.Pop()
+	testPeek(t, s, "a")
 
-        s.Pop()
-        testPeek(t, s, nil)
+	s.Pop()
+	testPeek(t, s, nil)
 }
 
 func TestLen(t *testing.T) {
-        s := New(iterations / 4)
+	s := New(iterations / 4)
 
-        for i := 0; i < iterations; i++ {
-                s.Push(i)
-        }
+	for i := 0; i < iterations; i++ {
+		s.Push(i)
+	}
 
-        if l := s.Len(); l != iterations {
-                t.Errorf("Stack length was expected to be %v, but is %v", iterations, l)
-        }
+	if l := s.Len(); l != iterations {
+		t.Errorf("Stack length was expected to be %v, but is %v", iterations, l)
+	}
 
-        s.Pop()
-        if l := s.Len(); l != iterations-1 {
-                t.Errorf("Stack length was expected to be %v, but is %v", iterations-1, l)
-        }
+	s.Pop()
+	if l := s.Len(); l != iterations-1 {
+		t.Errorf("Stack length was expected to be %v, but is %v", iterations-1, l)
+	}
 }
 
 func TestIsEmpty(t *testing.T) {
-        s := New(2)
+	s := New(2)
 
-        if s.IsEmpty() != true {
-                t.Errorf("Stack should be empty")
-        }
+	if s.IsEmpty() != true {
+		t.Errorf("Stack should be empty")
+	}
 
-        s.Push(1)
+	s.Push(1)
 
-        if s.IsEmpty() != false {
-                t.Errorf("Stack should not be empty")
-        }
+	if s.IsEmpty() != false {
+		t.Errorf("Stack should not be empty")
+	}
 }
 
 func testPop(t *testing.T, s *stack, e interface{}) {
-        if v := s.Pop(); v != e {
-                t.Errorf("Popping expected %v, got %v", e, v)
-        }
+	if v := s.Pop(); v != e {
+		t.Errorf("Popping expected %v, got %v", e, v)
+	}
 }
 
 func testPeek(t *testing.T, s *stack, e interface{}) {
-        if v := s.Peek(); v != e {
-                t.Errorf("Peeking expected %v, got %v", e, v)
-        }
+	if v := s.Peek(); v != e {
+		t.Errorf("Peeking expected %v, got %v", e, v)
+	}
 }
