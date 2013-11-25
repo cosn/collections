5837f43d2069e208861acc06e10a7af6e61fba4c
Cosmin Nicolaescu <cosmin.nicolaescu@gmail.com>
2013-11-24 16:06:06 -0800

    update GoSublime to use spaces instea of tabs

--- queue/queue.go
+++ queue/queue.go
@@ -5,39 +5,39 @@ import "container/list"
 
 // queue is the internal representation of the data structure
 type queue struct {
-	l *list.List
+        l *list.List
 }
 
 // New returns an initialized queue
 func New() (s *queue) {
-	s = new(queue)
-	s.l = list.New()
-	return
+        s = new(queue)
+        s.l = list.New()
+        return
 }
 
 // Push enqueues an element to the queue
 // O(1)
 func (s *queue) Push(v interface{}) {
-	s.l.PushFront(v)
+        s.l.PushFront(v)
 }
 
 // Pop dequeues an element from the queue
 // O(1)
 func (s *queue) Pop() interface{} {
-	if s.l.Len() == 0 {
-		return nil
-	}
+        if s.l.Len() == 0 {
+                return nil
+        }
 
-	v := s.l.Back()
-	return s.l.Remove(v)
+        v := s.l.Back()
+        return s.l.Remove(v)
 }
 
 // Len returns the number of elements in the queue
 func (s *queue) Len() int {
-	return s.l.Len()
+        return s.l.Len()
 }
 
 // IsEmpty returns a value indicating whether the queue has any elements
 func (s *queue) IsEmpty() bool {
-	return s.l.Len() == 0
+        return s.l.Len() == 0
 }
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -5,50 +5,50 @@ import "testing"
 const iterations = 1000
 
 func TestPushPop(t *testing.T) {
-    q := New()
+        q := New()
 
-    for i := 0; i < iterations; i++ {
-        q.Push(i)
-    }
+        for i := 0; i < iterations; i++ {
+                q.Push(i)
+        }
 
-    for i := 0; i < iterations; i++ {
-        testPop(t, q, i)
-    }
+        for i := 0; i < iterations; i++ {
+                testPop(t, q, i)
+        }
 }
 
 func TestLen(t *testing.T) {
-    q := New()
+        q := New()
 
-    for i := 0; i < iterations; i++ {
-        q.Push(i)
-    }
+        for i := 0; i < iterations; i++ {
+                q.Push(i)
+        }
 
-    if l := q.Len(); l != iterations {
-        t.Errorf("Queue length was expected to be %v, but is %v", iterations, l)
-    }
+        if l := q.Len(); l != iterations {
+                t.Errorf("Queue length was expected to be %v, but is %v", iterations, l)
+        }
 
-    q.Pop()
-    if l := q.Len(); l != iterations-1 {
-        t.Errorf("Queue length was expected to be %v, but is %v", iterations-1, l)
-    }
+        q.Pop()
+        if l := q.Len(); l != iterations-1 {
+                t.Errorf("Queue length was expected to be %v, but is %v", iterations-1, l)
+        }
 }
 
 func TestIsEmpty(t *testing.T) {
-    q := New()
+        q := New()
 
-    if q.IsEmpty() != true {
-        t.Errorf("Queue should be empty")
-    }
+        if q.IsEmpty() != true {
+                t.Errorf("Queue should be empty")
+        }
 
-    q.Push(1)
+        q.Push(1)
 
-    if q.IsEmpty() != false {
-        t.Errorf("Queue should not be empty")
-    }
+        if q.IsEmpty() != false {
+                t.Errorf("Queue should not be empty")
+        }
 }
 
 func testPop(t *testing.T, q *queue, e interface{}) {
-    if v := q.Pop(); v != e {
-        t.Errorf("Popping expected %v, got %v", e, v)
-    }
+        if v := q.Pop(); v != e {
+                t.Errorf("Popping expected %v, got %v", e, v)
+        }
 }
